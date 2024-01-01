73ee50da14f124650b29aa11cc779dc25426be26
ramarfx <ramagamiing33@gmail.com>
2024-01-01 21:37:46 +0700

    fix datatable lagi

--- resources/js/datatable.js
+++ resources/js/datatable.js
@@ -6,13 +6,18 @@ let table = new DataTable('#postTable', {
     scrollX: true,
     autoWidth: false,
     lengthChange: false,
-    pageLength: 20,
+    pageLength: 10,
     pagingType: 'simple',
     columnDefs: [
         {
             className: 'title-col',
             targets: 0,
             width: '500px'
+        },
+        {
+            targets: -1,
+            orderable: false,
+            searchable: false
         }
     ]
 });
